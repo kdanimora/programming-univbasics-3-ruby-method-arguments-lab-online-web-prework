d1ba61c7470e07401b2682040e66b1280bbe5ed4
Karen Daniela <kdaniimora@gmail.com>
2020-01-12 13:10:22 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -9,7 +9,7 @@ end
 introduction_with_language("Dan", "Ember.js")
 
 def introduction_with_language_optional(name, language)
-  puts  "Hi, my name is #{name} and I am learning to program in #{language}"
+  puts  "Hi, my name is #{name} and I am learning to program in #{language}."
 end 
 introduction_with_language_optional("Edwin", "Ruby")
 
