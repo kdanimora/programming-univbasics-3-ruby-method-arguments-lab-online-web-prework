d8e513f131711e333539ba17b3b0950c9c1e7841
Karen Daniela <kdaniimora@gmail.com>
2020-01-12 13:00:34 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,7 +8,10 @@ puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
 introduction_with_language("Dan", "Ember.js")
 
-
+def introduction_with_language_optional(name, language)
+  puts  "Hi, my name is #{name} and I am learning to program in #{language}"
+end 
+introduction_with_language_optional("Edwin", "Ruby")
 
 def introduction(name)
 puts "Hi, my name is #{name}."
